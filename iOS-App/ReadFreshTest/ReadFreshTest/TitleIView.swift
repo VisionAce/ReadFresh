1df07e49b1dcccd9ec7a56007c7cd36da7fb576b
Jeff <ambitious4728@gmail.com>
2023-12-27 17:40:38 +0800

    Modify Picker

---
+++ iOS-App/ReadFreshTest/ReadFreshTest/TitleIView.swift
@@ -0,0 +1,18 @@
+//
+//  TitleIView.swift
+//  ReadFreshTest
+//
+//  Created by 褚宣德 on 2023/12/27.
+//
+
+import SwiftUI
+
+struct TitleIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TitleIView()
+}
---
+++ iOS-App/ReadFreshTest/ReadFreshTest/dayMessageView.swift
@@ -0,0 +1,18 @@
+//
+//  dayMessageView.swift
+//  ReadFreshTest
+//
+//  Created by 褚宣德 on 2023/12/27.
+//
+
+import SwiftUI
+
+struct dayMessageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    dayMessageView()
+}
