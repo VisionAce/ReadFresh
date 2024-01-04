0e70053891ba1e69c3cc46a4751a4661dcfc4f75
Jeff <ambitious4728@gmail.com>
2024-01-04 15:32:27 +0800

    Change HomeView to MessageView, Put DeveloperView to SettingView

---
+++ iOS-App/ReadFreshTest/ReadFreshTest/DeveloperView.swift
@@ -0,0 +1,18 @@
+//
+//  DeveloperView.swift
+//  ReadFreshTest
+//
+//  Created by 褚宣德 on 2024/1/4.
+//
+
+import SwiftUI
+
+struct DeveloperView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DeveloperView()
+}
